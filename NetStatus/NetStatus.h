6f8862c4cd4c8add2154967ac09db1310fd5d8c0
Mini <mini@e-tec.com.tw>
2020-03-02 09:50:10 +0800

    Added framework source and podspec

---
+++ NetStatus/NetStatus.h
@@ -0,0 +1,19 @@
+//
+//  NetStatus.h
+//  NetStatus
+//
+//  Created by E-TEC on 2020/2/27.
+//  Copyright © 2020 CIDT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetStatus.
+FOUNDATION_EXPORT double NetStatusVersionNumber;
+
+//! Project version string for NetStatus.
+FOUNDATION_EXPORT const unsigned char NetStatusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetStatus/PublicHeader.h>
+
+
